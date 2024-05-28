1d81efa525771f1b778a64d3182e4828f64d80fa
anna <annagumiho190423@gmail.com>
2024-05-28 20:44:47 +0300

    finished

--- main.js
+++ main.js
@@ -11,38 +11,42 @@ const userInfo = {
   email: "",
   age: 0,
 };
-
-Object.keys(userInfo).forEach((item, index) => {
-    let answer = prompt(`${questions[index]}`, `${defaultAnswers[index]}`);
-    userInfo[item] = item === "age" ?  (new Date().getFullYear() - Number(answer)): userInfo[item] = answer.replace(/ /g, "");
-});
-
 const userUl = document.getElementById("userInfo");
 
 const createLiElement = (conteiner, text) => {
   let liEl = document.createElement("li");
   liEl.innerHTML = text;
   conteiner.appendChild(liEl);
-}; 
+};
 
-createLiElement(userUl, `Full name: <span>${userInfo.name} ${userInfo.surname}</span>`)
+Object.keys(userInfo).forEach((item, index) => {
+  let answer = prompt(`${questions[index]}`, `${defaultAnswers[index]}`);
+  userInfo[item] =
+    item === "age"
+      ? new Date().getFullYear() - Number(answer)
+      : (userInfo[item] = answer.replace(/ /g, ""));
+});
+
+createLiElement(
+  userUl,
+  `Full name: <span>${userInfo.name} ${userInfo.surname}</span>`
+);
 
 if (userInfo.email.startsWith("@")) {
-    createLiElement(
-        userUl,
-        `Email: not valid email - <span>${userInfo.email}</span> (symbol @ find on first place)`
-      );
+  createLiElement(
+    userUl,
+    `Email: not valid email - <span>${userInfo.email}</span> (symbol @ find on first place)`
+  );
 } else if (userInfo.email.endsWith("@")) {
-    createLiElement(
-        userUl,
-        `Email: not valid email - <span>${userInfo.email}</span> (symbol @ find on last place)`
-      );
+  createLiElement(
+    userUl,
+    `Email: not valid email - <span>${userInfo.email}</span> (symbol @ find on last place)`
+  );
 } else if (!userInfo.email.includes("@")) {
-    createLiElement(
-        userUl,
-        `Email: not valid email - <span>${userInfo.email}</span> (symbol @ not find)`
-      );
+  createLiElement(
+    userUl,
+    `Email: not valid email - <span>${userInfo.email}</span> (symbol @ not find)`
+  );
 }
 
-createLiElement(userUl, `Age: <span>${userInfo.age}</span>`)
-
+createLiElement(userUl, `Age: <span>${userInfo.age}</span>`);
